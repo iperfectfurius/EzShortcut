859faa94c672784e48f406c81939708266707513
Adrian <agarciat@cifpfbmoll.eu>
2021-12-12 13:16:02 +0100

    Notifyicon enabled and work.

--- EzShortcut/Form1.Designer.cs
+++ EzShortcut/Form1.Designer.cs
@@ -28,20 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.lolToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pHPiniIISToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(161, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(300, 120);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipTitle = "EZShortcut V0.1";
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lolToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(110, 26);
+            // 
+            // lolToolStripMenuItem
+            // 
+            this.lolToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pHPiniIISToolStripMenuItem});
+            this.lolToolStripMenuItem.Name = "lolToolStripMenuItem";
+            this.lolToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.lolToolStripMenuItem.Text = "Abrir...";
+            // 
+            // pHPiniIISToolStripMenuItem
+            // 
+            this.pHPiniIISToolStripMenuItem.Name = "pHPiniIISToolStripMenuItem";
+            this.pHPiniIISToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.pHPiniIISToolStripMenuItem.Text = "PHP.ini(IIS)";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
+            this.ShowInTaskbar = false;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private NotifyIcon notifyIcon1;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem lolToolStripMenuItem;
+        private ToolStripMenuItem pHPiniIISToolStripMenuItem;
     }
 }
--- EzShortcut/Form1.cs
+++ EzShortcut/Form1.cs
@@ -1,15 +1,31 @@
+using System;
+using System.Diagnostics;
+
 namespace EzShortcut
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
+            this.ShowInTaskbar = false;
             InitializeComponent();
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+           
+        }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Hide();
+            notifyIcon1.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine("xd");
         }
     }
 }
